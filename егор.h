142a66f900c72acfb4735c42dc9f6237bd7b5f05
JLICS <157013623+JLICS@users.noreply.github.com>
2024-04-01 21:51:49 +1000

    Delete егор.h

--- егор.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-
-double **plus ( int N, int M, double **m, double **matrix2);
-
-double **minus( int N, int M, double **m, double **matrix2);
-
-double **mult( int N, int M, double **m, double **matrix2);
